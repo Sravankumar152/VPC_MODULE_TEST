2cd4da78b8306081f27dda81a748cc22f60926a6
Sravankumar152 <thanatisravankumar756@outlook.com>
2026-03-04 14:46:51 +0530

    updated outputs.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,19 @@
 output "vpc_id" {
   value = module.aws_vpc.vpc_id
 }
+
+output "ig_id" {
+  value = module.aws_vpc.ig_id
+}
+
+output "public_subnet_ids" {
+  value = module.aws_vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.aws_vpc.private_subnet_ids
+}
+
+output "database_subnet_ids" {
+  value = module.aws_vpc.database_subnet_ids
+}
