a37abb434e8b4deff5f71f7ab752e8f46076c4b1
Sravankumar152 <thanatisravankumar756@outlook.com>
2026-03-04 13:50:31 +0530

    peering enabled

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,11 @@
 module "aws_vpc" {
 
-    source = "git::https://github.com/Sravankumar152/VPC_Module.git?ref=main"
+    source = "git::https://github.com/Sravankumar152/VPC_Module.git?ref=master"
     project = var.project
     environment = var.environment
     cidr_block = var.cidr_block
     tags = var.tags
+    is_peering_required = true
 
   
 }
